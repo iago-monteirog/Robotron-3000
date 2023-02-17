1789c97da35995b4ba0cd501669bc591169afc39
iago-monteirog <iagomgarcia9000@gmail.com>
2023-02-17 12:11:00 -0300

    feat: adiciona atualização das estatisticas do robo

--- js/main.js
+++ js/main.js
@@ -1,15 +1,58 @@
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas": {
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 const controle = document.querySelectorAll("[data-controle]");
+const estatisticas = document.querySelectorAll("[data-estatistica]");
 
 controle.forEach((element) => {
-    element.addEventListener('click', (event) => handleData(event.target.dataset.controle, event.target.parentNode));
+    element.addEventListener('click', (event) => {
+        handleData(event.target.dataset.controle, event.target.parentNode);
+        updateStatistics(event.target.dataset.peca);
+    })
 })
 
 function handleData(operacao, controle) {
-    const peca = controle.querySelector('.controle-contador');
+    const peca = controle.querySelector('[data-contador]');
 
     if (operacao === "-") {
         peca.value = parseInt(peca.value) - 1;
     } else {
         peca.value = parseInt(peca.value) + 1
     }
+}
+
+function updateStatistics(peca) {
+    estatisticas.forEach ((element) => {
+        element.textContent = parseInt(element.textContent) + pecas[peca][element.dataset.estatistica]
+    })
 }
